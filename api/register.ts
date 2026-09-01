41b7bd932b2131ea23afc25a3251cba88067054d
jangpkinfo-ctrl <jangpkinfo@gmail.com>
2026-09-01 23:42:55 +0500

    Update register.ts

--- api/register.ts
+++ api/register.ts
@@ -5,7 +5,7 @@ function generateReferralCode(): string {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let code = '';
   for (let i = 0; i < 8; i++) {
-    code += chars[Math.floor(Math.random() * chars.length)];
+    code += chars[Math.floor(Math.random() * chars.length)]; 
   }
   return code;
 }
